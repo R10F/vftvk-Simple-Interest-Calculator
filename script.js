83014312e00bb47c50bf981ee8267fb7ce7e5fe0
R10F <farrellriofa@gmail.com>
2022-04-14 06:19:26 +0700

    finish

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 function compute() {
   var principal = document.getElementById("principal").value;
+
+  if (principal == "" || principal <= 0) {
+    alert("Enter a positive Number");
+    document.getElementById("principal").focus();
+    return;
+  }
+
   var rate = document.getElementById("rate").value;
+
   var years = document.getElementById("years").value;
+
   var interest = (principal * years * rate) / 100;
+
   var year = new Date().getFullYear() + parseInt(years);
+
+  document.getElementById("result").innerHTML =
+    "<br/> <br/> If you deposit " +
+    "<mark> " +
+    principal +
+    "</mark>" +
+    ",<br> at an interest rate of " +
+    "<mark>" +
+    rate +
+    " % </mark> " +
+    "<br> You will receive an amount of " +
+    "<mark>" +
+    interest +
+    "</mark>" +
+    ",<br> in the year " +
+    "<mark>" +
+    year +
+    "</mark>" +
+    "<br>";
 }
 
 function updateRate() {
   var rateval = document.getElementById("rate").value;
-  document.getElementById("rate_val").innerText = rateval + "%";
-
-  return rateval;
+  document.getElementById("rate_val").innerText = rateval;
 }
