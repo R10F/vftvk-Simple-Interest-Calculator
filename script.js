27b9f42d52f585e4f3c7488344ffaf8e29f4b2f0
R10F <farrellriofa@gmail.com>
2022-04-13 21:20:14 +0700

    little change

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
 function compute() {
   var principal = document.getElementById("principal").value;
-
   var rate = document.getElementById("rate").value;
-
   var years = document.getElementById("years").value;
-
   var interest = (principal * years * rate) / 100;
-
   var year = new Date().getFullYear() + parseInt(years);
-
-  document.getElementById("result").innerHTML =
-    "</br> If you deposit " +
-    "<mark> " +
-    principal +
-    "</mark>" +
-    ",<br> at an interest rate of " +
-    "<mark>" +
-    rate +
-    " % </mark> " +
-    "<br> You will receive an amount of " +
-    "<mark>" +
-    interest +
-    "</mark>" +
-    ",<br> in the year " +
-    "<mark>" +
-    year +
-    "</mark>" +
-    "<br>";
 }
 
 function updateRate() {
